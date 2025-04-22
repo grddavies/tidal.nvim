58665dc493d1e31781819555bbfc013ca81f9283
Jan Fiedler <jbf@zuloo.de>
2025-04-22 21:39:17 +0200

    type hints and doc

--- lua/tidal/util/scd.lua
+++ lua/tidal/util/scd.lua
@@ -2,14 +2,20 @@ local state = require("tidal.core.state")
 
 local M = {}
 
+
+--- check if file exists
+--- @param file string filename
+--- @return boolean
 function M.file_exists(file)
   local f = io.open(file, "rb")
   if f then f:close() end
   return f ~= nil
 end
 
--- get all lines from a file, returns an empty 
--- list/table if the file does not exist
+--- get all lines from a file, returns an empty 
+--- list/table if the file does not exist
+--- @param file string filename
+--- @return table<string>
 function M.lines_from(file)
   if not M.file_exists(file) then return {} end
   local lines = {}
@@ -19,14 +25,20 @@ function M.lines_from(file)
   return lines
 end
 
+--- concatenates a table of strings
+--- for use with sclang repl
+--- @param lines table<string> table of strings to be concatenated
+--- @return string
 function M.scd_concat(lines)
   --- strip comments
   for k,v in pairs(lines) do
     lines[k] = v:gsub('//.*$', '')
   end
-  return table.concat(lines, " "):gsub('/%*.*%*/', '')
+  return table.concat(lines, " "):gsub('/%*.*%*/', '')[1]
 end
 
+--- return the current filetype and matching repl's proc
+--- @return string,any
 function M.filetype()
   local buf = vim.api.nvim_get_current_buf()
   local ftype = vim.api.nvim_get_option_value('filetype', { buf = buf })
