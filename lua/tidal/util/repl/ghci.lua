5dd10746e369271b82df29399675b469d1f57457
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-06-29 14:10:29 +0200

    fix(When writing on new lines and indenting with tab, the repl is not working ): sub \tab to space

--- lua/tidal/util/repl/ghci.lua
+++ lua/tidal/util/repl/ghci.lua
@@ -9,7 +9,9 @@ Ghci.__index = Ghci
 --- Send multi-line text to GHCi
 ---@param lines string[]
 function Ghci:send_multiline(lines)
-  return self:send_line(":{\n" .. table.concat(lines, "\n") .. "\n:}")
+	local send_str = ":{\n" .. table.concat(lines, "\n") .. "\n:}"
+	send_str = send_str:gsub("\t", " ")
+	return self:send_line(send_str)
 end
 
 return Ghci
